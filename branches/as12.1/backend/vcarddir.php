7dbcb7085c9772c6ecec3c8daff8b34853b2578b
dw2412 <dw2412@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2010-04-20 16:13:27 +0000

    (maildir) correction of GetMessage Parameter List (vcard)   correction of GetMessage Parameter List
    git-svn-id: https://www.z-push.org/svn/z-push@337 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- branches/as12.1/backend/vcarddir.php
+++ branches/as12.1/backend/vcarddir.php
@@ -111,7 +111,7 @@ class BackendVCDir extends BackendDiff {
         return $message;
     }
 
-    function GetMessage($folderid, $id, $truncsize, $mimesupport = 0) {
+    function GetMessage($folderid, $id, $truncsize, $bodypreference=false, $mimesupport = 0) {
         debugLog('VCDir::GetMessage('.$folderid.', '.$id.', ..)');
         if($folderid != "root")
             return;
