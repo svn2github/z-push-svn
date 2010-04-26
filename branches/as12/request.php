46c9872be7b6a9656b32692fd519d538e49e7126
dw2412 <dw2412@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2010-04-26 05:43:58 +0000

    ... this time fixed it right way ...
    git-svn-id: https://www.z-push.org/svn/z-push@359 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- branches/as12/request.php
+++ branches/as12/request.php
@@ -704,7 +704,7 @@ function HandleSync($backend, $protocolversion, $devid) {
                     $exporter = $backend->GetExporter($collection["collectionid"]);
 
                     $filtertype = isset($collection["filtertype"]) ? $collection["filtertype"] : false;
-                    $exporter->Config($importer, $collection["class"], $filtertype, $collection["syncstate"], 0, $collection["truncation"], $collection["BodyPreference"]);
+                    $exporter->Config($importer, $collection["class"], $filtertype, $collection["syncstate"], 0, $collection["truncation"], (isset($collection["BodyPreference"]) ? $collection["BodyPreference"] : false));
 
                     $changecount = $exporter->GetChangeCount();
             	}
