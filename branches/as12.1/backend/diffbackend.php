599617ebc3800872bfd923529163ebbc28cd7a92
dw2412 <dw2412@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2011-11-06 13:18:16 +0000

    (diffbackend) exclude SMS Messages from sync, clearify parameters in Exporter Config function call
    git-svn-id: https://www.z-push.org/svn/z-push@869 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- branches/as12.1/backend/diffbackend.php
+++ branches/as12.1/backend/diffbackend.php
@@ -363,7 +363,7 @@ class ExportChangesDiff extends DiffState {
     }
 
     // CHANGED dw2412 Support Protocol Version 12 (added bodypreference)
-    function Config(&$importer, $folderid, $restrict, $syncstate, $flags, $truncation, $bodypreference, $optionbodypreference, $mimesupport=0) {
+    function Config(&$importer, $mclass, $restrict, $syncstate, $flags, $truncation, $bodypreference, $optionbodypreference, $mimesupport) {
         $this->_importer = &$importer;
         $this->_restrict = $restrict;
         $this->_syncstate = unserialize($syncstate);
@@ -376,7 +376,6 @@ class ExportChangesDiff extends DiffState {
         $this->_changes = array();
         $this->_step = 0;
 
-		debugLog("DiffBackend::Config mimesupport is: ". $this->_mimesupport);
         $cutoffdate = $this->getCutOffDate($restrict);
 
         if($this->_folderid) {
@@ -391,8 +390,11 @@ class ExportChangesDiff extends DiffState {
             //do nothing if it is a dummy folder
             if ($this->_folderid != SYNC_FOLDER_TYPE_DUMMY) {
 
+				if ($mclass == 'syncsms') {
+					$this->_changes == array();
+				} else
                 // on ping: check if backend supports alternative PING mechanism & use it
-                if ($folderid === false && $this->_flags == BACKEND_DISCARD_DATA && $this->_backend->AlterPing()) {
+                if ($mclass === false && $this->_flags == BACKEND_DISCARD_DATA && $this->_backend->AlterPing()) {
                     $this->_changes = $this->_backend->AlterPingChanges($this->_folderid, $this->_syncstate);
                 }
                 else {
