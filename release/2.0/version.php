4251022772b4186abbd4a9a2da319407599cdb8b
mku <mku@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2012-06-12 19:35:06 +0000

    Z-Push 2.0RC  is rev 1333
    git-svn-id: https://www.z-push.org/svn/z-push@1333 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- release/2.0/version.php
+++ release/2.0/version.php
@@ -47,7 +47,7 @@ if (!defined("ZPUSH_VERSION")) {
         define("ZPUSH_VERSION", "SVN " . substr(trim($svn[4]),stripos($svn[4],"z-push")+7) ." r".trim($svn[3]));
     }
     else
-        define("ZPUSH_VERSION", "SVN checkout");
+        define("ZPUSH_VERSION", "2.0RC-1333");
 }
 
 ?>
