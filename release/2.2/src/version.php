68925fb4d45c2397e8a3c74c21c4bca089a6dfc8
mku <mku@b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5>
2014-10-22 19:33:58 +0000

    Z-Push 2.2.0beta1 is rev 1925
    git-svn-id: https://www.z-push.org/svn/z-push@1925 b7dd7b3b-3a3c-0410-9da9-bee62a6cc5b5

--- release/2.2/src/version.php
+++ release/2.2/src/version.php
@@ -41,6 +41,6 @@
 * Consult LICENSE file for details
 ************************************************/
 
-define("ZPUSH_VERSION", "2.2.0beta1-1919");
+define("ZPUSH_VERSION", "2.2.0beta1-1925");
 
 ?>
